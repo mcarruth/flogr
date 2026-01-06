626c3ad3f51a7041b9780f92fed3f31f7988b7f1
mcarruth <mikecarruth@gmail.com>
2026-01-06 09:15:05 -0700

    Updated footer

--- admin/footer.php
+++ admin/footer.php
@@ -5,7 +5,7 @@
         $photo->userlink();
         echo "&nbsp;|&nbsp;";
       }
-      ?>Site powered by <a href="http://mcarruth.github.io/flogr/">Flogr</a> &amp; <a href="http://flickr.com"><span style="color: rgb(0, 102, 204);">Flick</span><span style="color: rgb(255, 0, 153);">r</span></a>
+      ?>Site powered by <a href="https://github.com/mcarruth/flogr">Flogr</a> &amp; <a href="http://flickr.com"><span style="color: rgb(0, 102, 204);">Flick</span><span style="color: rgb(255, 0, 153);">r</span></a>
       </div>
     </body>
 </html>
